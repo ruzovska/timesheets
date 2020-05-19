aa38e0717978f21f6c53f431409d8e2d93803283
Ruzovska <nastybonda@gmail.com>
2020-05-19 18:11:58 +0300

    Add code that creates LaTeX.

--- Main.hs
+++ Main.hs
@@ -2,7 +2,9 @@
 
 module Main where
 
-import Data.Text
+import Text.LaTeX
+import Data.Text (Text)
+import qualified Data.Text.IO as Text
 import Data.Time.Clock
 import Data.Time.Format
 import Text.Pretty.Simple
@@ -10,8 +12,11 @@ import Text.Read
 
 main :: IO ()
 main = do
-    example <- readFile "example"
-    pPrint (read example :: [Entry])
+    example <- getContents
+    let entries = read example :: [Entry]
+    Text.putStrLn $ (render :: LaTeX -> Text) $ execLaTeXM $ do
+        documentclass [] article
+        document $ entriesToTable entries
 
 instance Read NominalDiffTime where
     readPrec = do
@@ -26,3 +31,7 @@ data Entry = Entry
     } deriving (Show, Read)
 
 data Ticket = Issue | PullRequest deriving (Show, Read)
+
+entriesToTable :: [Entry] -> LaTeXM ()
+entriesToTable xs = tabular Nothing [LeftColumn, LeftColumn] $ do
+    tex & tex
